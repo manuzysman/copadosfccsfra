224dd3e39271d0f3d88ada689620339a0e942c23
Emmanuel Zysman <emmanuelzysman@co-fdqr2nl9dc.home>
2024-04-29 23:49:05 +0200

    2nd cartridge

--- cartridges/bm_app_storefront_base/cartridge/experience/editors/custom/tileDisplayFormat.js
+++ cartridges/bm_app_storefront_base/cartridge/experience/editors/custom/tileDisplayFormat.js
@@ -4,5 +4,6 @@
  * Init for the displayFormat radio button custom editor
  *
  * Initialises the custom attribute editor with server side information. This editor does not need any.
+ * eeeeee
  */
 module.exports.init = function () {};
