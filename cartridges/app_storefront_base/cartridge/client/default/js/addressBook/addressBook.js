f0812cea792670379b881a1876ed2cb4dfb21db4
Emmanuel Zysman <emmanuelzysman@co-fdqr2nl9dc.home>
2024-06-20 09:05:12 +0200

    new

--- cartridges/app_storefront_base/cartridge/client/default/js/addressBook/addressBook.js
+++ cartridges/app_storefront_base/cartridge/client/default/js/addressBook/addressBook.js
@@ -6,7 +6,7 @@ var url;
 var isDefault;
 
 /**
- * Create an alert to display the error message
+ * Create an alert to display the error message ohohoho
  * @param {Object} message - Error message to display
  */
 function createErrorNotification(message) {
