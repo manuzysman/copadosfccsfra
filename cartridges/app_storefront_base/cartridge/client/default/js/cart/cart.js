d3428d6e8214fed4d4f6bd45c2d783ddff81a1d2
Emmanuel Zysman <emmanuelzysman@co-fdqr2nl9dc.home>
2024-08-29 12:18:42 +0200

    Test

--- cartridges/app_storefront_base/cartridge/client/default/js/cart/cart.js
+++ cartridges/app_storefront_base/cartridge/client/default/js/cart/cart.js
@@ -4,7 +4,7 @@ var base = require('../product/base');
 var focusHelper = require('../components/focus');
 
 /**
- * appends params to a url
+ * appends params to a url hello
  * @param {string} url - Original url
  * @param {Object} params - Parameters to append
  * @returns {string} result url with appended parameters
